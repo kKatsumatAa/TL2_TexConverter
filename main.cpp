8a6d1557d47590321ba92f5c8555cf4cede1dcba
イイダ_ユウキ <k021g1333@g.neec.ac.jp>
2023-10-02 21:07:37 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
+#include<memory>
 
-
-int main()
+int main(uint32_t argc, char* argv[])
 {
-	printf("Hello,World!\n");
+	//argc‚Ì”ŒJ‚è•Ô‚·
+	for (uint32_t i = 0; i < argc; i++)
+	{
+		//•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
